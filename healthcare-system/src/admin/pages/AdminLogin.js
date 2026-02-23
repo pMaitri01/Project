411484514baa764303b3cc36dc91ac45c7394b37
Maitri <maitrip306@gmail>
2026-02-23 11:37:57 +0530

    mistakenly deleted for correct folder structure

---
+++ healthcare-system/src/admin/pages/AdminLogin.js
@@ -0,0 +1,58 @@
+import React, { useState } from "react";
+import "./AdminLogin.css";
+
+export default function AdminLogin() {
+  const [formData, setFormData] = useState({
+    email: "",
+    password: "",
+  });
+
+  const handleChange = (e) => {
+    setFormData({ ...formData, [e.target.name]: e.target.value });
+  };
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    console.log(formData);
+  };
+
+  return (
+    <div className="doctor-login-container">
+      <div className="login-card">
+        <h2>Doctor Login</h2>
+
+        <form onSubmit={handleSubmit}>
+          <div className="input-group">
+            <label>Email</label>
+            <input
+              type="email"
+              name="email"
+              placeholder="Enter your email"
+              value={formData.email}
+              onChange={handleChange}
+              required
+            />
+          </div>
+
+          <div className="input-group">
+            <label>Password</label>
+            <input
+              type="password"
+              name="password"
+              placeholder="Enter password"
+              value={formData.password}
+              onChange={handleChange}
+              required
+            />
+          </div>
+
+          <button type="submit" className="login-btn">
+            Login
+          </button>
+
+          <p className="forgot">Forgot Password?</p>
+        </form>
+      </div>
+    </div>
+  );
+}
