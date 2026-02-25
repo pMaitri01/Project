97f84915c840ebe93b459d5c00f678d5b8e86728
kishanpancholi12 <kp.developer04@gmail.com>
2026-02-25 14:13:36 +0530

    solved showing warning error

--- healthcare-system/src/patient/pages/Login.js
+++ healthcare-system/src/patient/pages/Login.js
@@ -1,5 +1,5 @@
 import React, { useState } from "react";
-import { Navigate, useNavigate } from "react-router-dom";
+import { useNavigate } from "react-router-dom";
 import { FaEye, FaEyeSlash } from "react-icons/fa";
 import "./Login.css";
 import patientImage from "../images/patient-1.jpg";
